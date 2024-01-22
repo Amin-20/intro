918ce2afd298744cf473cbe8c91b04b60fafca9b
Amin <amin.atakisiyev1@gmail.com>
2024-01-22 11:56:49 +0400

    init

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import {ListGroup,ListGroupItem} from "reactstrap";
+import './SideBar.css';
+
 
 export default class SideBar extends Component {
+    style={
+      backgroundColor:"#883BFF",
+      color:"white",
+      border:"0px",
+      
+    }
+
   render() {
     return (
       <div>
-        
+        <ListGroup>
+          <ListGroupItem style={this.style} >Bootstrap Side Bar</ListGroupItem>
+          <ListGroupItem style={this.style} >Dapibus ac facilisis in</ListGroupItem>
+          <ListGroupItem style={this.style} >Morbi leo risus</ListGroupItem>
+          <ListGroupItem style={this.style} >Porta ac consectetur ac</ListGroupItem>
+          <ListGroupItem style={this.style} >Vestibulum at eros</ListGroupItem>
+        </ListGroup>
       </div>
-    )
+    );
   }
 }
