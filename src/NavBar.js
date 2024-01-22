d8a9a97f72f8507f19c7ac7ff6604ee44adff061
Ataki_Pw11 <Ataki_Pw11@STHQ0123-11>
2024-01-22 15:56:30 +0400

    Yes

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import { faAlignLeft } from "@fortawesome/free-solid-svg-icons";
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default class NavBar extends Component {
+  style = {};
+
   render() {
     return (
       <div>
-        
+        <Navbar className="my-1" color="dark" dark>
+          <NavbarBrand href="/">
+          <FontAwesomeIcon icon={faAlignLeft} />
+          </NavbarBrand>
+        </Navbar>
       </div>
-    )
+    );
   }
 }
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,25 +1,58 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem} from "reactstrap";
-import './SideBar.css';
-
+import { ListGroup, ListGroupItem } from "reactstrap";
+import "./SideBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faFile, faImage, faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+import {
+  faCaretDown,
+  faHome,
+  faInfo,
+  faQuestion,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default class SideBar extends Component {
-    style={
-      backgroundColor:"#883BFF",
-      color:"white",
-      border:"0px",
-      
-    }
+  style = {
+    backgroundColor: "#7386d7",
+    color: "white",
+    border: "0px",
+    width: "23%",
+    borderRadius: "0px",
+    fontSize: "20px",
+    cursor:'pointer',
+    textAlign:"left",
+    fontWeight:"bold",
+    height:"50px"
+  };
+
+  style2 = {
+    width: "23%",
+    backgroundColor: "#6d7fcc",
+    textAlign: "left",
+    color: "white",
+    height: "200px",
+  };
+
+  styleIcon={
+    paddingRight:"20px"
+  }
 
   render() {
     return (
       <div>
+        <div style={this.style2}>
+          <h1 style={{ marginLeft: "30px", paddingTop: "40px"}}>Bootstrap</h1>
+          <h1 style={{ marginLeft: "30px" }}>Sidebar</h1>
+        </div>
         <ListGroup>
-          <ListGroupItem style={this.style} >Bootstrap Side Bar</ListGroupItem>
-          <ListGroupItem style={this.style} >Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem style={this.style} >Morbi leo risus</ListGroupItem>
-          <ListGroupItem style={this.style} >Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem style={this.style} >Vestibulum at eros</ListGroupItem>
+          <ListGroupItem style={this.style} id="first">
+            Dummy Heading
+          </ListGroupItem>
+          <ListGroupItem style={this.style}><FontAwesomeIcon style={this.styleIcon} icon={faHome} /> Home <FontAwesomeIcon style={{marginLeft:"230px"}} icon={faCaretDown} /></ListGroupItem>
+          <ListGroupItem style={this.style}><FontAwesomeIcon style={this.styleIcon} icon={faInfo} /> About</ListGroupItem>
+          <ListGroupItem style={this.style}><FontAwesomeIcon style={this.styleIcon} icon={faFile} />  Pages <FontAwesomeIcon style={{marginLeft:"240px"}} icon={faCaretDown} /></ListGroupItem>
+          <ListGroupItem style={this.style}><FontAwesomeIcon style={this.styleIcon} icon={faImage} /> Portfolio</ListGroupItem>
+          <ListGroupItem style={this.style}><FontAwesomeIcon style={this.styleIcon} icon={faQuestion} /> FAQ</ListGroupItem>
+          <ListGroupItem style={this.style} id="last"><FontAwesomeIcon icon={faPaperPlane} /> Contact</ListGroupItem>
         </ListGroup>
       </div>
     );
