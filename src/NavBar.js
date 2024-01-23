0c22c502109e01d96d235aab3f1ce65fb1187e56
Amin Atakishiyev <amin.atakisiyev1@gmail.com>
2024-01-24 01:34:07 +0400

    Dvm

--- src/NavBar.js
+++ src/NavBar.js
@@ -8,16 +8,28 @@ export default class NavBar extends Component {
     position: "absolute",
     marginLeft:"24%",
     marginTop:"14px",
+    width:"75.5%",
+    border:"3px solid"
 
     
   };
 
+  style2={
+    backgroundColor:"#47a3b9",
+    padding:"15px"
+
+  };
+
+
   render() {
     return (
       <div style={this.style}>
-        <Navbar className="my-1" color="dark" dark>
+        <Navbar className="my-1" >
           <NavbarBrand href="/">
-          <FontAwesomeIcon icon={faAlignLeft} />
+            <div style={this.style2}>
+           <FontAwesomeIcon icon={faAlignLeft} style={{color:"#fff"}}/>
+              
+            </div>
           </NavbarBrand>
         </Navbar>
       </div>
